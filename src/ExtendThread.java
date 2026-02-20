6ee09b22f009bf6b6fe2ac1399670e00cdfc8f11
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-20 11:06:54 +0530

    Override run() method and start thread in Main
    Updated MyThread class to override run() method and start the thread in Main class.

--- src/ExtendThread.java
+++ src/ExtendThread.java
@@ -1,19 +1,12 @@
 class MyThread extends Thread {
-    // TODO: Override the run() method
-    void run(){
+    public void run() {
         System.out.println("Thread is running");
-        
     }
-    // Inside run(), print "Thread is running"
 }
 
-public class ExtendThread {
+public class Main {
     public static void main(String[] args) {
-
-        MyThread a=new MyThread();
-        start.thread();
-        
-        // TODO: Create an object of MyThread
-        // TODO: Start the thread using start() method
+        MyThread t = new MyThread();
+        t.start();
     }
 }
