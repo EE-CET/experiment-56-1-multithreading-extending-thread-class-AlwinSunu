7479e220fd2722e032e4defd3ecc0b88bfd8039d
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-20 11:09:45 +0530

    Rename Main class to ExtendThread

--- src/ExtendThread.java
+++ src/ExtendThread.java
@@ -1,10 +1,11 @@
 class MyThread extends Thread {
+    @Override
     public void run() {
         System.out.println("Thread is running");
     }
 }
 
-public class Main {
+public class ExtendThread {
     public static void main(String[] args) {
         MyThread t = new MyThread();
         t.start();
